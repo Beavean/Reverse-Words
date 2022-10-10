e99e2fa91ec50c254b2105bbcc31363fd2b7b56b
Anthony Petrov <beavean@gmail.com>
2022-10-10 18:00:29 +0300

    change ReverseWordsViewModel to class

--- ReverseWords/ReverseWordsViewModel.swift
+++ ReverseWords/ReverseWordsViewModel.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct ReverseWordsViewModel {
+class ReverseWordsViewModel {
     
     private let defaultAllowedCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
     
