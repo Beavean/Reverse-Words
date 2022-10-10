b7451b933af0a0cf59d0488e04b5ca775dc56f28
Anthony Petrov <beavean@gmail.com>
2022-10-10 16:24:11 +0300

    add new elements to UI and new reverse method to model

--- ReverseWords/ReverseWordsViewController.swift
+++ ReverseWords/ReverseWordsViewController.swift
@@ -15,12 +15,14 @@ final class ReverseWordsViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var resultLabel: UILabel!
     @IBOutlet weak var reverseButton: UIButton!
     @IBOutlet weak var textLabelBottomLineView: UIView!
-    @IBOutlet weak var reverseButtonBottomConstraint: NSLayoutConstraint!
+    @IBOutlet weak var filterSegmentedControl: UISegmentedControl!
+    @IBOutlet weak var customFilterTextField: UITextField!
+    @IBOutlet weak var defaultInformationLabel: UILabel!
     
     //MARK: - Properties
     
     private var processedString: String?
-    private let reverseButtonConstraintConstant: CGFloat = 40
+    private var filteredText: String?
     private let viewModel = ReverseWordsViewModel()
     
     //MARK: - Lifecycle
@@ -35,33 +37,53 @@ final class ReverseWordsViewController: UIViewController, UITextFieldDelegate {
     
     private func configure() {
         enteredStringTextField.addTarget(self, action: #selector(textFieldDidChange), for: .editingChanged)
+        customFilterTextField.addTarget(self, action: #selector(textFieldDidChange), for: .editingChanged)
         resultLabel.text = ""
     }
     
     private func setupKeyboardHiding() {
-        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
-        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
         let tap = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
         view.addGestureRecognizer(tap)
     }
     
     private func processEnteredString() {
         guard let enteredString = enteredStringTextField.text, !enteredString.isEmpty else { return }
-        if enteredString == processedString {
+        if enteredString == processedString && customFilterTextField.text == filteredText {
             clearInput()
         } else {
+            switch filterSegmentedControl.selectedSegmentIndex {
+            case 1:
+                resultLabel.text = viewModel.reverseInput(string: enteredString, defaultFilter: false, customFilter: customFilterTextField.text)
+            default:
+                resultLabel.text = viewModel.reverseInput(string: enteredString, defaultFilter: true)
+            }
             processedString = enteredStringTextField.text
-            resultLabel.text = viewModel.reversedString(enteredString)
+            filteredText = customFilterTextField.text
         }
         textFieldDidChange()
     }
     
     private func clearInput() {
         enteredStringTextField.text = ""
+        customFilterTextField.text = ""
         resultLabel.text = ""
+        enteredStringTextField.becomeFirstResponder()
     }
     
     //MARK: - Actions
+    
+    @IBAction func filterSegmentedControlChanged() {
+        switch filterSegmentedControl.selectedSegmentIndex {
+        case 1:
+            customFilterTextField.isHidden = false
+            defaultInformationLabel.isHidden = true
+        default:
+            customFilterTextField.isHidden = true
+            defaultInformationLabel.isHidden = false
+        }
+        processedString = ""
+        reverseButton.setTitle("Reverse", for: .normal)
+    }
     
     @IBAction func reverseButtonPressed() {
         processEnteredString()
@@ -73,25 +95,11 @@ final class ReverseWordsViewController: UIViewController, UITextFieldDelegate {
     }
     
     @objc
-    private func keyboardWillShow() {
-        reverseButtonBottomConstraint.isActive = false
-        reverseButtonBottomConstraint = reverseButton.topAnchor.constraint(equalTo: textLabelBottomLineView.bottomAnchor, constant: reverseButtonConstraintConstant + reverseButton.frame.height)
-        reverseButtonBottomConstraint.isActive = true
-    }
-    
-    @objc
-    private func keyboardWillHide() {
-        reverseButtonBottomConstraint.isActive = false
-        reverseButtonBottomConstraint = reverseButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -reverseButtonConstraintConstant)
-        reverseButtonBottomConstraint.isActive = true
-    }
-    
-    @objc
     private func textFieldDidChange() {
         let textIsEntered = enteredStringTextField.text?.isEmpty == false
         reverseButton.isEnabled = textIsEntered
         textLabelBottomLineView.backgroundColor = textIsEntered ? .tintColor : .separator
-        let reverseButtonTitle = enteredStringTextField.text == processedString ? "Clear" : "Reverse"
+        let reverseButtonTitle = enteredStringTextField.text == processedString && customFilterTextField.text == filteredText ? "Clear" : "Reverse"
         reverseButton.setTitle(reverseButtonTitle, for: .normal)
     }
     
--- ReverseWords/ReverseWordsViewModel.swift
+++ ReverseWords/ReverseWordsViewModel.swift
@@ -9,9 +9,33 @@ import Foundation
 
 struct ReverseWordsViewModel {
     
-    func reversedString(_ string: String) -> String {
-        let parts = string.components(separatedBy: " ")
-        let reversed = parts.map { String($0.reversed()) }
-        return reversed.joined(separator: " ")
+    private let defaultAllowedCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    
+    func reverseInput(string: String, defaultFilter: Bool, customFilter: String? = "") -> String {
+        var resultWords = [String]()
+        let words = string.components(separatedBy: " ")
+        for word in words {
+            let filterElements = defaultFilter ? Array(defaultAllowedCharacters) : Array(customFilter ?? "")
+            let characters = Array(word)
+            var enumeratedAndFilteredCharacters = [Character]()
+            var filteredCharacters = [Int: Character]()
+            for (index, character) in characters.enumerated() {
+                if defaultFilter ? !filterElements.contains(character) : filterElements.contains(character) {
+                    filteredCharacters[index] = character
+                } else {
+                    enumeratedAndFilteredCharacters.append(character)
+                }
+            }
+            var reversedArrayOfCharacters = Array(enumeratedAndFilteredCharacters.reversed())
+            for index in 0...string.count {
+                if let filteredCharacter = filteredCharacters[index] {
+                    reversedArrayOfCharacters.insert(filteredCharacter, at: index)
+                }
+            }
+            let joinedWord = reversedArrayOfCharacters.map{ String($0) }
+            let newJoinedWord = joinedWord.joined()
+            resultWords.append(newJoinedWord)
+        }
+        return resultWords.joined(separator: " ")
     }
 }
